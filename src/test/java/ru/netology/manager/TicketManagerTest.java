695b4f84a90c824ecfec04bd02c2a472c8c02709
Purpurova-k <katrin.purpurova@gmail.com>
2022-01-26 18:17:56 +0300

    Tests for comparator added

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.Test;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
+import java.util.Comparator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketManagerTest {
     private TicketRepository repository = new TicketRepository();
     private TicketManager manager = new TicketManager(repository);
+    private TicketByPriceAscComparator comparator = new TicketByPriceAscComparator();
 
-    private Ticket ticketSpbMoscow = new Ticket(1, 2165, "LED", "DME", 105);
+    private Ticket ticketSpbMoscow = new Ticket(1, 2165, "LED", "DME", 95);
     private Ticket ticketSpbMoscow2 = new Ticket(2, 3165, "LED", "DME", 105);
-    private Ticket ticketSpbMoscow3 = new Ticket(3, 4165, "LED", "DME", 105);
+    private Ticket ticketSpbMoscow3 = new Ticket(3, 4165, "LED", "DME", 115);
     private Ticket ticketSochiSpb = new Ticket(4, 5105, "AER", "LED", 185);
     private Ticket ticketStavropolRnd = new Ticket(5, 8350, "STW", "ROV", 115);
     private Ticket ticketSpbNovosibirsk = new Ticket(6, 4100, "LED", "OVB", 255);
@@ -60,6 +63,22 @@ class TicketManagerTest {
 
         Ticket[] actual = manager.findAll("LED", "STW");
         Ticket[] expected = {};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldFindAllTicketsSortByFlightTime() {
+        manager.add(ticketSpbMoscow2);
+        manager.add(ticketSpbMoscow3);
+        manager.add(ticketSochiSpb);
+        manager.add(ticketSpbMoscow);
+        manager.add(ticketSpbNovosibirsk);
+        manager.add(ticketStavropolRnd);
+
+        Ticket[] actual = manager.findAll("LED", "DME", comparator);
+        Ticket[] expected = {ticketSpbMoscow, ticketSpbMoscow2, ticketSpbMoscow3};
 
         assertArrayEquals(expected, actual);
     }
